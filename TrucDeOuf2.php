ce990ac5c689c255471cf7e69ba2e8249811a873
Joris Calabrese <joris.calabrese@gmail.com>
2014-07-18 00:12:44 +0200

    Comment code

---
+++ TrucDeOuf2.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joriscalabrese
+ * Date: 18/07/2014
+ * Time: 00:07
+ */
+
+class TrucDeOuf {
+    protected $leTrucDeOuf;
+
+    /**
+     * Do the sum of the two given parameter
+     *
+     * @param $a Integer
+     * @param $b Integer
+     * @return Integer Sum of the parameters
+     *
+     */
+    public function trouveTonTrucDeOuf ($a, $b)
+    {
+        return $a + $b ;
+    }
+} 
